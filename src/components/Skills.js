d572ee875a28f5d2c18e0db42c1c7091dc87bf62
Saibal kole <saibalkole@gmail.com>
2024-07-31 18:29:27 +0530

    adds skillcard carousel functionallity and makes it responsive

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -17,14 +17,42 @@ const responsive = {
     items: 3,
   },
   tablet: {
-    breakpoint: { max: 1024, min: 464 },
+    breakpoint: { max: 1024, min: 661 },
     items: 2,
   },
   mobile: {
-    breakpoint: { max: 464, min: 0 },
+    breakpoint: { max: 661, min: 0 },
     items: 1,
   },
 };
+
+const skillCardBrkPoint = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 8,
+    partialVisibilityGutter: 40
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1300},
+    items: 6,
+  },
+  desktop2: {
+    breakpoint: { max: 1300, min: 1080},
+    items: 5,
+  },
+  tablet: {
+    breakpoint: { max: 1080, min: 861 },
+    items: 4,
+  },
+  tablet2:{
+    breakpoint:{ max: 861, min: 661 },
+    items: 3,
+  },
+  mobile: {
+    breakpoint: { max: 661, min: 0 },
+    items: 2,
+  },
+}
 
 const Skills = () => {
   const [isDesktop,setIsDesktop] = useState(window.innerWidth>1030)
@@ -36,18 +64,16 @@ const Skills = () => {
         <DetailsCard/>
         <DetailsCard/>
       </Carousel>
-      {/* <div className="detlailsContainer">
-        <DetailsCard />
-        <DetailsCard />
-        <DetailsCard />
-      </div> */}
       <div className="skillContainer">
+      <Carousel responsive={skillCardBrkPoint} showDots={false} arrows={true} infinite={true} autoPlay={true} autoPlaySpeed={3500}>
+
         <SkillCard title={"Node js"} icon={icons.nodeJs} />
         <SkillCard title={"React js"} icon={icons.react} />
         <SkillCard title={"Mongo Db"} icon={icons.mongo} />
         <SkillCard title={"Express Js"} icon={icons.express} />
         <SkillCard title={"Next Js"} icon={icons.next} />
         <SkillCard title={"Tailwind"} icon={icons.tailwind} />
+      </Carousel>
       </div>
     </div>
   );
